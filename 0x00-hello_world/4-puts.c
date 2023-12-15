815ce29e7328627aa1d4f417d4d2110e1de454b2
Denzoz <d.wambua@yahoo.com>
2023-12-15 08:23:49 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.>
+/**
+ * main -outputs a line
+ *
+ * Return: 0 (succesfully)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n,")
+return (0);
+}
