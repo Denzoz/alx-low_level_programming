d0a50b5a9d18c88b434a63360534821b2b2b9ce1
Denzoz <d.wambua@yahoo.com>
2024-01-03 17:22:48 -0800

    meme

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: input string
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
